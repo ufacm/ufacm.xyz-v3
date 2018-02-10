3034b52439741f12e72dbd662b96dd432ac6fc45
garyg1 <garygurlaskie@gmail.com>
2018-02-10 13:34:48 -0500

    fixed newlines, removed deprecated files

--- react/src/indexDeprecated.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Homepage from './components/HomepageDeprecated.jsx';
-import { Button } from 'semantic-ui-react';
-
-ReactDOM.render(
-  <Homepage />,
-  document.getElementById('root')
-);
